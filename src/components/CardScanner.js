0804ac421c4dcc510f8ecc2055c8f9273ac28612
Nadejde <andrei.nadejde@gmail.com>
2020-12-12 12:20:53 +0000

    card detection

--- src/components/CardScanner.js
+++ src/components/CardScanner.js
@@ -1,44 +1,106 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import Spinner from './Spinner'
+import Images from './Images'
+import Buttons from './Buttons'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-function CardScanner(props) {
-    const { window, children } = props;
 
-    const [starsCount, setStarsCount] = useState([])
-    useEffect(() => {
-    // get data from GitHub api
+
+export default class App extends Component {
+  state = {
+    uploading: false,
+    images: [],
+    collections: [],
+    numbers: [],
+    selectedCollection: ""
+  };
+
+  componentDidMount() {
     fetch('https://nadejde-collector-api.azurewebsites.net/api/collections')
-        .then(response => response.json()) // parse JSON from request
-        .then(resultData => {
-        setStarsCount(resultData)
-        console.log(resultData)
-        console.log(starsCount)
-        }) // set data for the number of stars
-    }, [])
+    .then(response => response.json()) // parse JSON from request
+    .then(resultData => {
+      this.setState({
+        collections: resultData
+      });
+      console.log(resultData)
+    }) // set data for the number of stars
+  }
+
+  onChange = (e) => {
+    const files = Array.from(e.target.files);
+    this.setState({ uploading: true });
+
+    const formData = new FormData();
+
+    files.forEach((file, i) => {
+      formData.append(i, file);
+    });
+
+    fetch(`https://nadejde-collector-api.azurewebsites.net/api/collections/Panini - Football 2020/detect-numbers`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((resultData) => {
+        this.setState({
+          uploading: false,
+          numbers: [...this.state.numbers,...resultData]
+        });
+      });
+  };
+
+  removeImage = (id) => {
+    this.setState({
+      images: this.state.images.filter((image) => image.public_id !== id),
+    });
+  };
+
+  render() {
+    const { uploading, images, numbers, collections} = this.state;
+
+   
+    const content = () => {
+      switch (true) {
+        case uploading:
+          return <Spinner />;
+        default:
+          return <Buttons onChange={this.onChange} />;
+      }
+    };
 
     return (
-    <div>
+      <div>
         <FormControl>
         <InputLabel id="demo-simple-select-label">Collection</InputLabel>
         <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
+            value={this.state.selectedCollection}
         >
             {
-            starsCount.map((collection) => {
+            collections.map((collection) => {
                 return (<MenuItem value={collection.id}>{collection.name}</MenuItem>)
             })
             }
         </Select>
-        <input type="file" accept="image/*"></input>
         </FormControl>
-    </div>
-
-    )
+        <div className="buttons">{content()}</div>
+        <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          fullWidth
+          value={numbers}
+        />
+         <Button variant="contained" color="primary">Upload</Button>
+      </div>
+    );
+  }
 }
 
-export default CardScanner;
